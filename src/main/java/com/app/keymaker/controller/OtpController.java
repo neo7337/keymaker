a9b15c67c52ef9379b7485d9f82447291aace919
Nidhi Mann <nidhimnn@thoughtworks.com>
2019-05-05 06:24:04 +0530

    blockchain integration

--- src/main/java/com/app/keymaker/controller/OtpController.java
+++ src/main/java/com/app/keymaker/controller/OtpController.java
@@ -1,8 +1,13 @@
 package com.app.keymaker.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.app.keymaker.Block;
+import com.app.keymaker.Transaction;
+import com.app.keymaker.TransactionOutput;
+import com.app.keymaker.Wallet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +29,9 @@ import com.mongodb.DBCollection;
 
 @Controller
 public class OtpController {
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    Block currentBlock ;
+    public static int difficulty = 0;
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -83,12 +91,85 @@ public class OtpController {
 
     public String generateAuthKey(String email, String otp) {
 
+        Wallet customerWallet = new Wallet();
         String SUCCESS = "Success";
         String FAIL = "Fail";
-
+        Block block;
+        Wallet newWallet = new Wallet();
+        int count = 0;
         KeyGenerator keyGen = new KeyGenerator();
         String generatedKey = keyGen.randomKeyGenerator();
+        Map<String,String> newUserData = new HashMap<String, String>();
+        newUserData.put(email,otp);
+        Transaction newTransaction = new Transaction(newWallet.publicKey,customerWallet.publicKey,(HashMap<String, String>)newUserData);
+        newTransaction.generateSignature(newWallet.privateKey);
+        newTransaction.transactionId = "0";
+        newTransaction.outputs.add(new TransactionOutput(newTransaction.reciepient, (HashMap<String, String>) newTransaction.userData, newTransaction.transactionId));
+        if (!newTransaction.verifiySignature())
+            return FAIL;
+        if (count == 0){
+            block = new Block("0");
+            currentBlock = block ;
+        }
+        else {
+            block = new Block(currentBlock.previousHash);
+            currentBlock = block;
+        }
 
-        return generatedKey;
+        block.addTransaction(newTransaction);
+        addBlock(currentBlock);
+
+        if (isChainValid()){
+            return generatedKey;
+        }
+        return FAIL;
+    }
+
+    public static void addBlock(Block newBlock) {
+        newBlock.mineBlock(difficulty);
+        blockchain.add(newBlock);
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        for(int i=1; i < blockchain.size(); i++) {
+
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("#Previous Hashes not equal");
+                return false;
+            }
+            if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+                System.out.println("#This block hasn't been mined");
+                return false;
+            }
+
+            TransactionOutput tempOutput;
+            for(int t=0; t <currentBlock.transactions.size()-1; t++) {
+                Transaction currentTransaction = currentBlock.transactions.get(t);
+
+                if(!currentTransaction.verifiySignature()) {
+                    System.out.println("#Signature on Transaction(" + t + ") is Invalid");
+                    return false;
+                }
+
+                if( currentTransaction.outputs.get(0).reciepient != currentTransaction.reciepient) {
+                    System.out.println("#Transaction(" + t + ") output reciepient is not who it should be");
+                    return false;
+                }
+                if( currentTransaction.outputs.get(1).reciepient != currentTransaction.sender) {
+                    System.out.println("#Transaction(" + t + ") output 'change' is not sender.");
+                    return false;
+                }
+
+            }
+
+        }
+        System.out.println("Blockchain is valid");
+        return true;
     }
 }
