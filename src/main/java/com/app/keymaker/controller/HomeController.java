23d928037cd8b09dd090abbc502d45b10a45ac21
Aditya <adityak007mr@gmail.com>
2019-06-05 03:16:45 +0530

    Revert "ReactJS Integrated"
    This reverts commit 8e749ede3d7f50fd6740da15ee3b256137cb394e.

--- src/main/java/com/app/keymaker/controller/HomeController.java
+++ src/main/java/com/app/keymaker/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.app.keymaker.controller;
 
-import java.util.Date;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String homePage() {
-        return "Hello, the time at the server is now " + new Date() + "\n";
+        return "index";
     }
 
     @GetMapping("/keygen")
--- src/main/java/com/app/keymaker/controller/OtpController.java
+++ src/main/java/com/app/keymaker/controller/OtpController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +25,7 @@ import com.app.keymaker.model.User;
 import com.app.keymaker.model.UserRepository;
 import com.app.keymaker.service.*;
 import com.app.keymaker.utility.*;
+import com.mongodb.DBCollection;
 
 @Controller
 public class OtpController {
@@ -110,6 +112,7 @@ public class OtpController {
     public String generateAuthKey(String email, String otp) {
 
         Wallet customerWallet = new Wallet();
+        String SUCCESS = "Success";
         String FAIL = "Fail";
         Block block;
         Wallet newWallet = new Wallet();
@@ -176,6 +179,7 @@ public class OtpController {
                 return false;
             }
 
+            TransactionOutput tempOutput;
             for (int t = 0; t < currentBlock.transactions.size() - 1; t++) {
                 Transaction currentTransaction = currentBlock.transactions.get(t);
 
@@ -192,7 +196,9 @@ public class OtpController {
                     System.out.println("#Transaction(" + t + ") output 'change' is not sender.");
                     return false;
                 }
+
             }
+
         }
         System.out.println("Blockchain is valid");
         return true;
